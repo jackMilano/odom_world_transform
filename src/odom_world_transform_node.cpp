e45ec8f44668d8e868f0a6a6935e29493ed15a33
Giacomo Milano <italiancasual@gmail.com>
2015-10-08 18:34:25 +0200

    Adesso pubblica la più recente trasformata, anche se non sono stati ricevuti messaggi.

--- src/odom_world_transform_node.cpp
+++ src/odom_world_transform_node.cpp
@@ -21,8 +21,8 @@
 
 #include <boost/bind.hpp>
 
-#define SYNC_POLICY_WINDOW_SIZE 15
 #define RAD2DEG 180.0 / 3.14159265359
+#define SYNC_POLICY_WINDOW_SIZE 15
 
 class OdomWorldTransformEstimator
 {
@@ -55,6 +55,8 @@ private:
   tf::TransformBroadcaster transform_bc_;
   tf::TransformListener tf_listener_;
   std::string base_frame_;
+  bool old_transform_set;
+  tf::Transform old_transform;
 };
 
 int main(int argc, char** argv)
@@ -91,7 +93,8 @@ int main(int argc, char** argv)
 
 OdomWorldTransformEstimator::OdomWorldTransformEstimator(double rate_hz, const std::string& robot_base_frame)
   : rate_(rate_hz), yaw_offset_calibrated_(false), yaw_offset_(0.0), base_frame_(robot_base_frame),
-    pose_sub_(nh_, "robot_pose", 1), odom_sub_(nh_, "odom", 1), pose_odom_sub_(SyncPolicy(SYNC_POLICY_WINDOW_SIZE))
+    pose_sub_(nh_, "robot_pose", 1), odom_sub_(nh_, "odom", 1), pose_odom_sub_(SyncPolicy(SYNC_POLICY_WINDOW_SIZE)),
+    old_transform_set(false)
 {
   pose_odom_sub_.connectInput(pose_sub_, odom_sub_);
 }
@@ -118,14 +121,14 @@ bool OdomWorldTransformEstimator::calibrateYawOffset(const tf::Vector3& velocity
   tf::vector3TFToMsg(velocity, twist.linear);
   ros::Duration duration(time_secs);
 
-  //register calibration callback
+  // register calibration callback
   message_filters::Connection conn = pose_odom_sub_.registerCallback(boost::bind(
                                        &OdomWorldTransformEstimator::yawOffsetCalibCallback, this, _1, _2));
 
   ros::Publisher cmd_vel_pub = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
-  //wait for initial pose
-  //start moving the robot
+  // wait for initial pose
+  // start moving the robot
   ros::Time finish(ros::Time::now() + duration);
 
   while(ros::Time::now() < finish)
@@ -191,13 +194,21 @@ void OdomWorldTransformEstimator::spin()
 
     while(nh_.ok())
     {
+      if(old_transform_set)
+      {
+        tf::StampedTransform stamped_trans = tf::StampedTransform(old_transform.inverse(), ros::Time(0), "world", "odom");
+        transform_bc_.sendTransform(stamped_trans);
+      }
+
+      // 'spinOnce': will call all the callbacks waiting to be called at that point in time.
+      // 'spinOnce': non dovrebbe essere bloccante.
       ros::spinOnce();
       rate_.sleep();
     }
   }
   else
   {
-    ROS_ERROR("Yaw offset is not calibrated yet. You must call calibrateYawOffset() first. Aborting...");
+    ROS_ERROR("Yaw offset is not calibrated yet. You must call calibrateYawOffset() first. Aborting ...");
   }
 
   return;
@@ -227,8 +238,10 @@ void OdomWorldTransformEstimator::estimateTransformCallback(const geometry_msgs:
     // pose of base_footprint wrt world
     tf::Transform base_to_world(tf::createQuaternionFromYaw(yaw_offset_), tf::Vector3(pose->pose.pose.position.x,
                                 pose->pose.pose.position.y, 0.0));
+
     // pose of world wrt base_footprint
     tf::Stamped<tf::Pose> world_to_base_stamped(base_to_world.inverse(), pose->header.stamp, base_frame_);
+
     // pose of world wrt odom
     tf_listener_.transformPose(odom->header.frame_id, world_to_base_stamped, odom_to_world);
   }
@@ -239,16 +252,20 @@ void OdomWorldTransformEstimator::estimateTransformCallback(const geometry_msgs:
   }
   catch(const std::exception& ex)
   {
-    ROS_ERROR("%s", ex.what());
+    ROS_ERROR("exception: %s", ex.what());
     ros::Duration(1.0).sleep();
   }
 
   tf::Transform transform(tf::Quaternion(odom_to_world.getRotation()), tf::Point(odom_to_world.getOrigin()));
 
+  // salvataggio dati vecchi
+  old_transform = transform;
+  old_transform_set = true;
+
   //TODO: publish future stamped??
   // inverse to obtain pose of odom wrt world (world to odom transform)
-  transform_bc_.sendTransform(tf::StampedTransform(transform.inverse(), pose->header.stamp, pose->header.frame_id,
-                              odom->header.frame_id));
+  tf::StampedTransform stamped_trans = tf::StampedTransform(old_transform.inverse(), pose->header.stamp, "world", "odom");
+  transform_bc_.sendTransform(stamped_trans);
 
   return;
 }
